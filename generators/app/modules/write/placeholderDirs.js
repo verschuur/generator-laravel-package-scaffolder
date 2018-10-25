c4d0fc2125b5812ff4175c4a6ef98d05c3c5b33a
Govert Verschuur <govert.verschuur@gmail.com>
2018-10-25 23:01:37 +0200

    Renamed placeholder directories .gitignore to .gitignore.placeholders in preparation for the main .gitignore addition

--- generators/app/modules/write/placeholderDirs.js
+++ generators/app/modules/write/placeholderDirs.js
@@ -11,7 +11,7 @@ module.exports = function(generator) {
     "public/"
   ].forEach(function(item) {
     generator.fs.copy(
-      generator.templatePath("_.gitignore"),
+      generator.templatePath("_.gitignore.placeholders"),
       generator.destinationPath(item + "/.gitignore")
     );
   });
